42ec8969f1f0a4a5f803d3e925cadcaf2a861802
JL <jldeleage@free.fr>
2016-11-06 07:47:57 +0100

    Default factory methods
    newList and newSet have been added.

--- src/main/java/fr/insset/jeanluc/util/factory/FactoryMethods.java
+++ src/main/java/fr/insset/jeanluc/util/factory/FactoryMethods.java
@@ -5,6 +5,8 @@
  */
 package fr.insset.jeanluc.util.factory;
 
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
@@ -22,6 +24,15 @@ public class FactoryMethods {
     public  static  <T>   List<T>   newList(Class<T> inClass) throws InstantiationException {
         FactoryRegistry registry = FactoryRegistry.getRegistry();
         AbstractFactory factory = registry.getFactory(LIST);
+        if (factory == null) {
+            synchronized (registry) {
+                factory = registry.getFactory(LIST);
+                if (factory == null) {
+                    factory = new FactoryRegistryImpl.DefaultFactory(LinkedList.class);
+                    registry.registerFactory(LIST, factory);
+                }       // factory == null
+            }       // synchronized
+        }       // factory == null
         return (List<T>)factory.newInstance();
     }
 
@@ -29,6 +40,15 @@ public class FactoryMethods {
     public  static  <T>   Set<T>   newSet(Class<T> inClass) throws InstantiationException {
         FactoryRegistry registry = FactoryRegistry.getRegistry();
         AbstractFactory factory = registry.getFactory(SET);
+        if (factory == null) {
+            synchronized (registry) {
+                factory = registry.getFactory(SET);
+                if (factory == null) {
+                    factory = new FactoryRegistryImpl.DefaultFactory(HashSet.class);
+                    registry.registerFactory(SET, factory);
+                }
+            }       // synchronized
+        }       // factory == null
         return (Set<T>)factory.newInstance();
     }
 
